54783f0c2a3102f302bd6b3a679b866fc65a3ef2
Thomas Laukkanen <thomas.laukkanen@etraveligroup.com>
2025-04-27 23:04:50 +0200

    add book

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className=" font-[family-name:var(--font-geist-sans)]">
       <main className=" w-full h-full">
         <h1 className="text-5xl font-bold text-center mt-60">Logga in för att se loggar</h1>
+        <div className="text-8xl mx-auto w-fit mt-8">📘</div>
       </main>
     </div>
   );
